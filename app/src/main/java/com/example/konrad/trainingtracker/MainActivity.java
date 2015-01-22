7b8c8b0de6bf77775542f57f4d6cc2a9ae3858d0
Konrad Kopciuch <konrad.kopciuch@comarch.pl>
2015-01-22 12:33:05 +0100

    dodawanie markera, przesuwanie kamery

--- app/src/main/java/com/example/konrad/trainingtracker/MainActivity.java
+++ app/src/main/java/com/example/konrad/trainingtracker/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.konrad.trainingtracker;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity  {
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        if(map!=null){
+            LatLng x = new LatLng(50,19);
+            Marker lastMarkup = map.addMarker(new MarkerOptions()
+                    .position(x));
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(x, 15));
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
     }
 }
